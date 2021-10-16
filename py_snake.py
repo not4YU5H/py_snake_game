e3375ac0bd1a17037b2c17fb2b54762fafa6a74d
Ayush Jain <77970015+not4YU5H@users.noreply.github.com>
2021-10-16 16:17:44 +0530

    Added eat sound and reset function.

--- py_snake.py
+++ py_snake.py
@@ -6,7 +6,7 @@ from pygame.math import Vector2
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
-        self.direction = Vector2(1,0)
+        self.direction = Vector2(0,0)
         self.new_block = False
 
         self.head_up = pygame.image.load('img/head_up.png').convert_alpha()
@@ -26,6 +26,8 @@ class SNAKE:
         self.body_tl = pygame.image.load('img/body_tl.png').convert_alpha()
         self.body_br = pygame.image.load('img/body_br.png').convert_alpha()
         self.body_bl = pygame.image.load('img/body_bl.png').convert_alpha()
+
+        self.crunch_sound = pygame.mixer.Sound('sounds/EatCrispHardCrunch.mp3')
 
     def draw_snake(self):
 
@@ -100,6 +102,12 @@ class SNAKE:
     def add_block(self):
         self.new_block = True
 
+    def play_sound(self):
+        self.crunch_sound.play()
+
+    def reset(self):
+        self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
+        self.direction = Vector2(0,0)
 
 class FRUIT:
     def __init__(self):
@@ -138,6 +146,11 @@ class MAIN:
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
             self.snake.add_block()
+            self.snake.play_sound()
+
+        for block in self.snake.body[1:]:
+            if block == self.fruit.pos:
+                self.fruit.randomize
 
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
@@ -148,8 +161,9 @@ class MAIN:
                 self.game_over()
 
     def game_over(self):
-        pygame.quit()
-        sys.exit()
+        # pygame.quit()
+        # sys.exit()
+        self.snake.reset()
 
     def draw_grass(self):
         grass_color = (167,209,61)
@@ -180,7 +194,7 @@ class MAIN:
         screen.blit(apple,apple_rect)
         pygame.draw.rect(screen,(56,74,12),bg_rect,2)
 
-
+pygame.mixer.pre_init(44100,-16,2,512)
 pygame.init()
 cell_size = 40
 cell_number = 20
